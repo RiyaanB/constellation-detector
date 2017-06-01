6516b1987e43c0087ce3cb610638283e5c8b144f
Riyaan <riyaanbakhda2@gmail.com>
2017-06-01 23:03:16 +0530

    Added calibration script

--- calibration.py
+++ calibration.py
@@ -1,9 +1,13 @@
 import cv2
-import numpy as np;
+import numpy as np
+import time
 class StarDetector:
     @staticmethod
     def getStarBlobs(im):
         im = cv2.erode(im,np.ones((2,2)))
+
+        height, width = im.shape[:2]
+        area = height * width
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
          
@@ -13,8 +17,10 @@ class StarDetector:
          
         # Filter by Area.
         params.filterByArea = True
-        params.minArea = 49
-        params.maxArea = 1024
+        #10000
+        #500
+        params.minArea = area/6596.4
+        params.maxArea = area/1
          
         # Filter by Circularity
         params.filterByCircularity = True
@@ -24,6 +30,7 @@ class StarDetector:
         # Filter by Convexity
         params.filterByConvexity = False
         params.minConvexity = 0.1
+        params.maxConvexity = 1
          
         # Filter by Inertia
         params.filterByInertia = True
@@ -58,19 +65,34 @@ class StarDetector:
         im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         height, width = im_with_keypoints.shape[:2]
         cv2.imshow(name,cv2.resize(im_with_keypoints,(int(factor*width), int(factor*height)), interpolation = cv2.INTER_CUBIC))
-        print(keypoints)
+        print(len(keypoints))
     @staticmethod
     def getKeypoints(name):
         im = 255 - cv2.imread(name, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow("test "+name,im)
+        #cv2.imshow("test "+name,im)
         msk = cv2.inRange(im,0,196)
         im = 255 - cv2.bitwise_and(255-im,255-im,mask=msk)
         keypoints = StarDetector.blobSieve(StarDetector.getStarBlobs(im))
         StarDetector.displayBlobs(im,keypoints,1,name)
-# Read image
 
-StarDetector.getKeypoints("orion.jpg")
-StarDetector.getKeypoints("big_dipper.jpg")
+cv2.namedWindow('image')
 
-cv2.waitKey(0)
+# create trackbars for color change
+cv2.createTrackbar('minThr','image',0,255,nothing)
+cv2.createTrackbar('maxThr','image',0,255,nothing)
+cv2.createTrackbar('minAre','image',0,255,nothing)
+cv2.createTrackbar('maxAre','image',0,255,nothing)
+cv2.createTrackbar('minCir','image',0,255,nothing)
+cv2.createTrackbar('maxCir','image',0,255,nothing)
+cv2.createTrackbar('minCon','image',0,255,nothing)
+cv2.createTrackbar('maxCon','image',0,255,nothing)
+cv2.createTrackbar('minIne','image',0,255,nothing)
+cv2.createTrackbar('minMsk','image',0,255,nothing)
+cv2.createTrackbar('maxMsk','image',0,255,nothing)
+
+while True:
+    StarDetector.getKeypoints("orion.jpg")
+    StarDetector.getKeypoints("big_dipper.jpg")
+    StarDetector.getKeypoints("leo.jpg")
+    cv2.waitKey(0)
 quit()
--- main.py
+++ main.py
@@ -1,48 +1,84 @@
 import cv2
 import numpy as np
-params = cv2.SimpleBlobDetector_Params()
-# Change thresholds
-params.minThreshold = 10;
-params.maxThreshold = 200;
- 
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 1500
- 
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.1
- 
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.87
- 
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.01
+import time
+class StarDetector:
+    @staticmethod
+    def getStarBlobs(im):
+        im = cv2.erode(im,np.ones((2,2)))
 
-blobVer = (cv2.__version__).split('.')
-if int(blobVer[0]) < 3:
-    detector = cv2.SimpleBlobDetector(params)
-else:
-    detector = cv2.SimpleBlobDetector_create(params)
+        height, width = im.shape[:2]
+        area = height * width
+        # Setup SimpleBlobDetector parameters.
+        params = cv2.SimpleBlobDetector_Params()
+         
+        # Change thresholds
+        params.minThreshold = 4;
+        params.maxThreshold = 196;
+         
+        # Filter by Area.
+        params.filterByArea = True
+        #10000
+        #500
+        params.minArea = area/6596.4
+        params.maxArea = area/1
+         
+        # Filter by Circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.1
+        params.maxCircularity = 1
+         
+        # Filter by Convexity
+        params.filterByConvexity = False
+        params.minConvexity = 0.1
+        params.maxConvexity = 1
+         
+        # Filter by Inertia
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.3
+
+        ver = (cv2.__version__).split('.')
+        if int(ver[0]) < 3 :
+            detector = cv2.SimpleBlobDetector(params)
+        else : 
+            detector = cv2.SimpleBlobDetector_create(params)
+         
+        keypoints = detector.detect(im)
+        return keypoints#,im
+    @staticmethod
+    def printKeypoints(keypoints):
+        for keypoint in keypoints:
+            print(keypoint.size)
+    @staticmethod
+    def blobSieve(keypoints):
+        keypoints = sorted(keypoints,key=lambda s:s.size)
+        if len(keypoints) > 12:
+            th = keypoints[len(keypoints)//4].size + 1
+            new = []
+            for keypoint in keypoints:
+                if keypoint.size > th:
+                    new.append(keypoint)
+            return new
+        return keypoints
     
-frame = cv2.imread("BlobTest.jpg",cv2.IMREAD_GRAYSCALE)
+    @staticmethod
+    def displayBlobs(image,keypoints,factor,name):
+        im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        height, width = im_with_keypoints.shape[:2]
+        cv2.imshow(name,cv2.resize(im_with_keypoints,(int(factor*width), int(factor*height)), interpolation = cv2.INTER_CUBIC))
+        print(len(keypoints))
+    @staticmethod
+    def getKeypoints(name):
+        im = 255 - cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+        #cv2.imshow("test "+name,im)
+        msk = cv2.inRange(im,0,196)
+        im = 255 - cv2.bitwise_and(255-im,255-im,mask=msk)
+        keypoints = StarDetector.blobSieve(StarDetector.getStarBlobs(im))
+        StarDetector.displayBlobs(im,keypoints,1,name)
 
-keypoints = detector.detect(255 - frame)
-len(keypoints)
-for keypoint in keypoints:
-    print(keypoint.pt[0],keypoint.pt[1],keypoint.size)
-im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imshow("foo",im_with_keypoints)
+
 while True:
-    c = cv2.waitKey(0)
-    if  c == -1:
-        break
-cv2.destroyAllWindows()
-
-"""
-print("out")
-keypoints = detector.detect(frame)
-print(keypoints)
-"""
+    StarDetector.getKeypoints("orion.jpg")
+    StarDetector.getKeypoints("big_dipper.jpg")
+    StarDetector.getKeypoints("leo.jpg")
+    cv2.waitKey(0)
+quit()
